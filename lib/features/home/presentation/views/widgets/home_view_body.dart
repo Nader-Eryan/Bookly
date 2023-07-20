1b2c30dee3303196190895a07a3d5cc755dd5286
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-07-20 21:33:50 +0300

    separated custom app bar

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,10 +1,6 @@
-import 'package:e_book/core/utils/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:size_config/size_config.dart';
+
+import 'custom_app_bar.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -14,30 +10,6 @@ class HomeViewBody extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: const [CustomAppBar()],
-      ),
-    );
-  }
-}
-
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 26.w, vertical: 70.h),
-      child: Row(
-        children: [
-          SvgPicture.asset(AsssetsData.logo),
-          const Spacer(),
-          Icon(
-            FontAwesomeIcons.magnifyingGlass,
-            size: 32.h,
-          ),
-          SizedBox(
-            width: 4.w,
-          )
-        ],
       ),
     );
   }
