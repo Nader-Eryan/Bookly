3ecf1fdfb3c5a881ffb0f7822710ab445d862c5e
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-07-22 15:42:28 +0300

    changed a font

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -4,7 +4,7 @@ abstract class Styles {
   static const textStyle30 =
       TextStyle(fontSize: 30, fontWeight: FontWeight.w500);
   static const textStyle20 =
-      TextStyle(fontSize: 20, fontWeight: FontWeight.normal);
+      TextStyle(fontSize: 20, fontWeight: FontWeight.w600);
   static const textStyle18 =
       TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
   static const textStyle16 =
