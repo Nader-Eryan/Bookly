8024a96d5e163e2b0232662dee079c571993df5e
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-07-22 14:19:13 +0300

    added font families

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff100B20);
 const kTransitionDuration = Duration(seconds: 1);
+const kGTSectraFine = 'GT Sectra Fine';
+const kGilroyBold = 'Gilroy-Bold';
