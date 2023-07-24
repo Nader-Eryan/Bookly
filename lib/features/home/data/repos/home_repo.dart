9cac5ef6f74eea275d3335563458e0679a6f0b28
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-07-24 22:01:52 +0300

    spcified the return type of home repo & created failure abstract class

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_book/core/errors/failure.dart';
+import 'package:e_book/features/home/data/models/book_model/book_model.dart';
+
 abstract class HomeRepo {
-  fetchFeaturedBooks();
-  fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
 }
