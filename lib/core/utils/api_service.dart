067bdada53e4059839d769f35063e6e3e6c804c2
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-07-25 10:42:01 +0300

    create get api service

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final _baseUrl = '';
+  final Dio dio;
+
+  ApiService(this.dio);
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    var respone = await dio.get('$_baseUrl$endPoint');
+    return respone.data;
+  }
+}
