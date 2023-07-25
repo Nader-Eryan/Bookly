65b9aa4d1418baec9ca5ed3ea3fcb29cf7ecf2b5
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-07-25 11:09:46 +0300

    implement fetch newest books

--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -1,12 +1,27 @@
+import 'package:e_book/core/utils/api_service.dart';
 import 'package:e_book/features/home/data/models/book_model/book_model.dart';
 import 'package:e_book/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:e_book/features/home/data/repos/home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  HomeRepoImpl(this.apiService);
+  final ApiService apiService;
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?q=self-improvements+algorithims+bussiness+novel+life+motivation+education&filter=free-ebooks');
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      return left(ServerFailure());
+    }
     throw UnimplementedError();
   }
 
