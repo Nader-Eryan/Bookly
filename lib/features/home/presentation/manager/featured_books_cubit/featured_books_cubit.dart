7f8927e703b4dd781d01b8f8f10af8fd9f846b6b
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-07-26 10:56:36 +0300

    created featured books method

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,23 @@
-import 'package:bloc/bloc.dart';
-import 'package:e_book/features/home/data/models/book_model/book_model.dart';
 import 'package:equatable/equatable.dart';
+
+import 'package:e_book/features/home/data/models/book_model/book_model.dart';
+import 'package:e_book/features/home/data/repos/home_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(
+    this.homeRepo,
+  ) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    return result.fold((failure) {
+      emit(FeaturedBooksFailure(errMessage: failure.errMessage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books: books));
+    });
+  }
 }
